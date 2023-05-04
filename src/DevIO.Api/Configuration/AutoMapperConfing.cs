12758e6673214b62067bfb1d7be778addd10e1dc
Diósney Gomes <diosney.gomes@beeit.com.br>
2023-05-04 11:33:39 -0300

    Aula - Setup - Controllers e Startup

---
+++ src/DevIO.Api/Configuration/AutoMapperConfing.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Configuration
+{
+    public class AutoMapperConfing : Profile
+    {
+        public AutoMapperConfing()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(
+            IFornecedorRepository fornecedorRepository,
+            IMapper mapper)
+        {
+            this._fornecedorRepository = fornecedorRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedores = this._mapper
+                .Map<IEnumerable<FornecedorViewModel>>(await this._fornecedorRepository
+                    .ObterTodos());
+
+            return fornecedores;
+        }
+    }
+}
