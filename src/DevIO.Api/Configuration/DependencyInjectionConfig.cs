ea1ebbaf9b121a59535ed2872ec1184a1c855d8e
Diósney Gomes <diosney.gomes@beeit.com.br>
2023-05-08 16:50:23 -0300

    Aula - Testando o resultado com Postman

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 
@@ -8,8 +10,20 @@ namespace DevIO.Api.Configuration
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
+            // Contexto
             services.AddScoped<MeuDbContext>();
+
+            // Repositórios
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            // Services
+            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
+
+            // Notificações
+            services.AddScoped<INotificador, Notificador>();
 
             return services;
         }
