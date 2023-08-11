59b7669ded4595357335114312e0bb86acfbacbb
Diósney Gomes <diosney.gomes@beeit.com.br>
2023-08-11 16:47:07 -0300

    Aula - Realizando o deploy no Azure

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -6,6 +6,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
